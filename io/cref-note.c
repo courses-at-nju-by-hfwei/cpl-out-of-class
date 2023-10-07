c39382743fcabb6fd12a2de8b99804f7f53bd137
hfwei <hfwei@nju.edu.cn>
2023-10-07 20:38:24 +0800

    +cref-note

---
+++ io/cref-note.c
@@ -0,0 +1,21 @@
+//
+// Created by hfwei on 2023/9/21.
+//
+
+#include <stdio.h>
+
+int main(void) {
+  int a = 0;
+  int b = 0;
+  char c = 'a';
+
+  scanf("%d", &a);
+  scanf("%d", &b);
+  printf("a = %d, b = %d\n", a, b);
+
+  scanf("%d", &a);
+  scanf(" %c", &c); // consume all consecutive whitespace after %d, then read a char
+  printf("a = %d, c = %c\n", a, c);
+
+  return 0;
+}
