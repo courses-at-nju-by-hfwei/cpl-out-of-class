b60ea5b60db0afdc6872beb74b877699f740765c
hfwei <hfwei@nju.edu.cn>
2023-10-09 08:08:13 +0800

    +code-style/ugly-elegant.c

---
+++ code-style/ugly-elegant.c
@@ -0,0 +1,34 @@
+//
+// Created by hfwei on 2023/10/8.
+//
+
+#include <stdio.h>
+
+int main(void) {
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  scanf("%d%d%d", &a, &b, &c);
+
+
+  // TODO: calculate the minimum of a, b and c
+  int min = 0;
+
+  if (a > b) {
+    if (b > c) {
+      min = c;
+    } else {
+      min = b;
+    }
+  } else { // a <= b
+    if (a < c) {
+      min = a;
+    } else {
+      min = c;
+    }
+  }
+
+  printf("min(%d, %d, %d) = %d\n", a, b, c, min);
+
+  return 0;
+}
