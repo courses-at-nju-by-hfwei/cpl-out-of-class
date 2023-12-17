ff8a17017c41f91b3b88d8486c789bed8d063c01
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2023-12-17 16:01:36 +0800

    pointers-malloc.c: +links to visualization

--- pointers/pointers-malloc.c
+++ pointers/pointers-malloc.c
@@ -5,12 +5,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LEN 5
+#define LEN 3
 #define ROW 3
-#define COL 5
+#define COL 4
 
 int main(void) {
   // (1) One-Dimensional Array
+  // Visualization: https://pythontutor.com/render.html#code=%23include%20%3Cstdio.h%3E%0A%23include%20%3Cstdlib.h%3E%0A%0A%23define%20LEN%203%0A%0A//%20%281%29%20One-Dimensional%20Array%0Aint%20main%28void%29%20%7B%0A%20%20int%20*array%20%3D%20malloc%28LEN%20*%20sizeof%20*array%29%3B%0A%20%20if%20%28array%20%3D%3D%20NULL%29%20%7B%0A%20%20%20%20printf%28%22malloc%20failed%5Cn%22%29%3B%0A%20%20%20%20return%20EXIT_FAILURE%3B%20%20//%20return%201%3B%0A%20%20%7D%0A%0A%20%20//%20fill%20in%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20LEN%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20array%5Bi%5D%20%3D%20i%20*%20i%3B%0A%20%20%7D%0A%0A%20%20//%20free%0A%20%20free%28array%29%3B%0A%0A%20%20return%200%3B%0A%7D&cppShowMemAddrs=true&cumulative=true&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=c_gcc9.3.0&rawInputLstJSON=%5B%5D&textReferences=false
   // malloc
   int *array = malloc(LEN * sizeof *array);
   if (array == NULL) {
@@ -33,6 +34,7 @@ int main(void) {
   free(array);
 
   // (2) Two-Dimensional Array
+  // Visualization: https://pythontutor.com/render.html#code=%23include%20%3Cstdlib.h%3E%0A%0A%23define%20ROW%203%0A%23define%20COL%204%0A%0A//%20%282%29%20Two-Dimensional%20Array%0Aint%20main%28void%29%20%7B%0A%20%20int%20%28*table%29%5BCOL%5D%20%3D%20malloc%28ROW%20*%20sizeof%20*table%29%3B%0A%0A%20%20//%20fill%20in%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20ROW%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20for%20%28int%20j%20%3D%200%3B%20j%20%3C%20COL%3B%20%2B%2Bj%29%20%7B%0A%20%20%20%20%20%20table%5Bi%5D%5Bj%5D%20%3D%20i%20*%20j%3B%0A%20%20%20%20%7D%0A%20%20%7D%0A%0A%20%20//%20free%0A%20%20free%28table%29%3B%0A%0A%20%20return%200%3B%0A%7D&cppShowMemAddrs=true&cumulative=true&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=c_gcc9.3.0&rawInputLstJSON=%5B%5D&textReferences=false
   // malloc
   int (*table)[COL] = malloc(ROW * sizeof *table);
 
@@ -56,6 +58,7 @@ int main(void) {
   free(table);
 
   // (3) One-Dimensional Array of Pointers
+  // Visualization: https://pythontutor.com/render.html#code=%23include%20%3Cstdlib.h%3E%0A%0A%23define%20LEN%203%0A%0A//%20%283%29%20One-Dimensional%20Array%20of%20Pointers%0Aint%20main%28void%29%20%7B%0A%20%20int%20*array_of_pointers%5BLEN%5D%3B%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20LEN%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20array_of_pointers%5Bi%5D%20%3D%20malloc%28%28i%20%2B%201%29%20*%20sizeof%20*array_of_pointers%5Bi%5D%29%3B%0A%20%20%20%20for%20%28int%20j%20%3D%200%3B%20j%20%3C%20i%20%2B%201%3B%20%2B%2Bj%29%20%7B%0A%20%20%20%20%20%20array_of_pointers%5Bi%5D%5Bj%5D%20%3D%20i%20*%20j%3B%0A%20%20%20%20%7D%0A%20%20%7D%0A%0A%20%20//%20free%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20LEN%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20free%28array_of_pointers%5Bi%5D%29%3B%0A%20%20%7D%0A%0A%20%20return%200%3B%0A%7D&cppShowMemAddrs=true&cumulative=true&curInstr=0&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=c_gcc9.3.0&rawInputLstJSON=%5B%5D&textReferences=false
   // malloc and fill in
   int *array_of_pointers[LEN];
   for (int i = 0; i < LEN; ++i) {
@@ -80,6 +83,7 @@ int main(void) {
   }
 
   // (4) Two-Dimensional Array with Potentially Non-Contiguous Memory
+  // Visualization: https://pythontutor.com/render.html#code=%23include%20%3Cstdlib.h%3E%0A%0A%23define%20ROW%203%0A%23define%20COL%204%0A%0A//%20%284%29%20Two-Dimensional%20Array%20with%20Potentially%20Non-Contiguous%20Memory%0Aint%20main%28void%29%20%7B%0A%20%20int%20**matrix%20%3D%20malloc%28ROW%20*%20sizeof%20*matrix%29%3B%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20ROW%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20matrix%5Bi%5D%20%3D%20malloc%28COL%20*%20sizeof%20*matrix%5Bi%5D%29%3B%0A%20%20%7D%0A%0A%20%20//%20fill%20in%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20ROW%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20for%20%28int%20j%20%3D%200%3B%20j%20%3C%20COL%3B%20%2B%2Bj%29%20%7B%0A%20%20%20%20%20%20matrix%5Bi%5D%5Bj%5D%20%3D%20i%20*%20j%3B%0A%20%20%20%20%7D%0A%20%20%7D%0A%0A%20%20//%20free%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20ROW%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20free%28matrix%5Bi%5D%29%3B%0A%20%20%7D%0A%20%20free%28matrix%29%3B%0A%0A%20%20return%200%3B%0A%7D&cppShowMemAddrs=true&cumulative=true&curInstr=48&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=c_gcc9.3.0&rawInputLstJSON=%5B%5D&textReferences=false
   // malloc
   int **matrix = malloc(ROW * sizeof *matrix);
   for (int i = 0; i < ROW; ++i) {
@@ -109,6 +113,7 @@ int main(void) {
   free(matrix);
 
   // (5) Two-Dimensional Array with Contiguous Memory
+  // Visualization: https://pythontutor.com/render.html#code=%23include%20%3Cstdlib.h%3E%0A%0A%23define%20ROW%203%0A%23define%20COL%204%0A%0A//%20%285%29%20Two-Dimensional%20Array%20with%20Contiguous%20Memory%0Aint%20main%28void%29%20%7B%0A%20%20int%20*matrix%20%3D%20malloc%28ROW%20*%20COL%20*%20sizeof%20*matrix%29%3B%0A%0A%20%20//%20fill%20in%0A%20%20for%20%28int%20i%20%3D%200%3B%20i%20%3C%20ROW%3B%20%2B%2Bi%29%20%7B%0A%20%20%20%20for%20%28int%20j%20%3D%200%3B%20j%20%3C%20COL%3B%20%2B%2Bj%29%20%7B%0A%20%20%20%20%20%20matrix%5Bi%20*%20COL%20%2B%20j%5D%20%3D%20i%20*%20j%3B%0A%20%20%20%20%7D%0A%20%20%7D%0A%0A%20%20//%20free%0A%20%20free%28matrix%29%3B%0A%0A%20%20return%200%3B%0A%7D&cppShowMemAddrs=true&cumulative=true&curInstr=35&heapPrimitives=nevernest&mode=display&origin=opt-frontend.js&py=c_gcc9.3.0&rawInputLstJSON=%5B%5D&textReferences=false
   // malloc
   int *matrix_contiguous = malloc(ROW * COL * sizeof *matrix_contiguous);
 
